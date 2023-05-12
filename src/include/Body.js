8e6c26483c3e9bcb1b54c0c2cbcef0fbf26df63e
hwang young il <anakyn@naver.com>
2023-05-12 18:53:03 +0900

    ss

--- src/include/Body.js
+++ src/include/Body.js
@@ -15,6 +15,205 @@ const Body = () => {
             </p>
         </div>
     </section>
+
+    <section className="promotion-section">
+        <div className="container">
+            <ul className="promo-list">
+                <li>
+                    <a href="#">
+                        <img src="images/s-images/promo01.png" alt=""/>
+                        <h3>HOME</h3>
+                        <p>Excepteur aute magna dolore ea ex do.</p>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="images/s-images/promo02.png" alt=""/>
+                        <h3>WE ARE</h3>
+                        <p>Excepteur aute magna dolore ea ex do.</p>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="images/s-images/promo03.png" alt=""/>
+                        <h3>WORK</h3>
+                        <p>Excepteur aute magna dolore ea ex do.</p>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="images/s-images/promo04.png" alt=""/>
+                        <h3>BLOG</h3>
+                        <p>Excepteur aute magna dolore ea ex do.</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <hr class="divider"/>
+
+    <section className="work-section cfixed">
+        <h2 className="sec-tit">
+            WORK
+        </h2>
+        <ul className="work-list">
+            
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Running</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work01.jpg" alt=""/>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Rugby</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work02.jpg" alt=""/>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Weight</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work03.jpg" alt=""/>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Marathon</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work04.jpg" alt=""/>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Boxing</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work05.jpg" alt=""/>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Ice Hockey</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work06.jpg" alt=""/>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Skate Board</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work07.jpg" alt=""/>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div className="info">
+                        <h3>Basketball</h3>
+                        <span>WEB/PRINT</span>
+                    </div>
+                    <img src="images/p-images/work08.jpg" alt=""/>
+                </a>
+            </li>
+
+
+        </ul>
+    </section>
+
+    <hr className="m-divider"/>
+
+    <section className="blog-section">
+        <div className="container">
+            <h2 className="sec-tit">BLOG</h2>
+            <ul className="blog-list">
+                
+                <li>
+                    <a href="#">
+                        <img src="images/p-images/blog01.jpg" alt=""/>
+                    </a>
+                    <time datetime="2023-05-10">
+                    May 10, 2023
+                    </time>
+                    <a href="#">
+                        <h3>
+                        Pariatur aute minim id cupidatat pariatur.
+                        </h3> 
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="images/p-images/blog02.jpg" alt=""/>
+                    </a>
+                    <time datetime="2023-05-10">
+                    May 10, 2023
+                    </time>
+                    <a href="#">
+                        <h3>
+                        Pariatur aute minim id cupidatat pariatur.
+                        </h3> 
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="images/p-images/blog03.jpg" alt=""/>
+                    </a>
+                    <time datetime="2023-05-10">
+                    May 10, 2023
+                    </time>
+                    <a href="#">
+                        <h3>
+                        Pariatur aute minim id cupidatat pariatur.
+                        </h3> 
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+<section className="contact-section">
+    <div className="container">
+        <h2 class="sec-tit">
+            CONTACT
+        </h2>
+
+        <div className="form-box">
+            <form action="ex.js" method="post">
+
+
+            </form>
+        </div>
+    </div>
+</section>
+
+
+
 </section>
         </>
     )
